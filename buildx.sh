2cb15e52c5cb3bb222c32e21aae0c141239dda6f
Alexey Cluster <cluster@cluster.wtf>
2025-11-30 16:04:20 +0100

    Add buildx script and update nightly workflow for multiarch support
    - Introduced a new buildx.sh script to facilitate multiarch Docker image builds.
    - Updated the nightly.yml workflow to include additional platforms for image building.
    
    Signed-off-by: Alexey Cluster <cluster@cluster.wtf>

---
+++ buildx.sh
@@ -0,0 +1,6 @@
+docker buildx create --name multi --use
+docker buildx inspect --bootstrap
+
+#docker login
+
+docker buildx build --platform linux/arm64,linux/arm/v7,linux/arm/v6,linux/arm/v5 -t clustermeerkat/pebble-dev-arm-linux:1.0 -t clustermeerkat/pebble-dev-arm-linux:latest --push .
